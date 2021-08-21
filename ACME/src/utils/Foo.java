7230ed3b555f1a3f447b906a15e4e7e6feda675a
Thuong Pham <timphamvn33@gmail.com>
2021-08-21 09:49:04 -0400

    hello git1

--- ACME/src/utils/Foo.java
+++ ACME/src/utils/Foo.java
@@ -5,6 +5,7 @@ public class Foo {
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("hi from GitHub");
+		System.out.println("hi 'pham1' branch, from Eclipse");
 
 	}
 
